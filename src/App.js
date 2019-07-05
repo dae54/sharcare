5db93fc865ebea674f626f8365b5640c1e95319c
dae54 <46072940+dae54@users.noreply.github.com>
2019-07-05 15:47:42 +0300

    first uploading of files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SingleFileUpload from './components/fileUploads/SingleFileUpload'
+
+function App() {
+  return (
+    <>
+      <SingleFileUpload />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/fileUploads/SingleFileUpload.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class SingleFileUpload extends Component {
+    constructor(){
+        super();
+        this.state={
+            selectedPic: {}
+        }
+        this.handleFileUPload = this.handleFileUPload.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleFileUPload(event){
+        this.setState({
+            selectedPic : event.target.files[0]
+        })
+    }
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state.selectedPic)
+        fetch('http://localhost:8000/upload',({
+            method:'POST',
+            'Content-TYpe': 'multipart/form-data',
+            // encType: 'multipart/form-data',
+            body:this.state.selectedPic
+        }))
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit} method='POST' encType='multipart/form-data'>
+                    example file input<br />
+                    <input type='file' name='myImage' onChange={this.handleFileUPload}/><br/><br/>
+                    <button type='submit' className='btn btn-light'>submit </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SingleFileUpload
